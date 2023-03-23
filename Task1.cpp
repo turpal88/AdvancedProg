e00bfb59087464749f62ca77d0959c995357dba4
Ivan Buldakov <pereslav88@gmail.com>
2023-03-23 15:39:58 +0300

    Second commit

--- Task1.cpp
+++ Task1.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 
 int main()
 {
     int a;
     std::vector<int> Vec = {4, 7, 9, 14, 12};
     std::cout << "Входные данные: ";
-    for(int el:Vec) std::cout << el << " ";
+    std::for_each(Vec.begin(), Vec.end(), [](int n){std::cout << n << " ";});
+    std::cout << std::endl;
+    std::cout << "Выходные данные: ";
+    std::for_each(Vec.begin(), Vec.end(), [](int n){
+       if(n % 2 != 0 || n == 1) std::cout << n * 3 << " ";
+       else std::cout << n << " ";
+
+
+    });
     return 0;
 }
