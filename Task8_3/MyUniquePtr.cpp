e882945bfe82778bbdca071521ae7f75bc414b91
turpal88 <pereslav88@gmail.com>
2023-05-05 11:38:16 +0300

    Task8(1,2,3)
    Task8(1,2,3)

---
+++ Task8_3/MyUniquePtr.cpp
@@ -0,0 +1,27 @@
+﻿template<typename T>
+class MyUniquePtr {
+
+public:
+	MyUniquePtr() : obj(nullptr) {}
+	MyUniquePtr(T* my_obj) : obj(my_obj) {
+		std::cout << "Создался объект класса MyUniquePtr" << std::endl;
+	}
+	
+	T operator*() {
+		std::cout << "Я объект класса MyUniquePtr и я вернул разыменованный объект" << std::endl;
+		return *obj;
+	}
+	MyUniquePtr& operator=(const MyUniquePtr& right) = delete;
+	MyUniquePtr(const MyUniquePtr& right) = delete;
+	T* release() {
+		std::cout << "Я объект класса MyUniquePtr и я вернул сырой указатель через свой метод release()" << std::endl;
+		return this->obj;
+	}
+	~MyUniquePtr() {
+		delete[] this->obj;
+		std::cout << "Я объект класса MyUniquePtr и я самоуничтожился" << std::endl;
+	}
+private:
+	T* obj;
+
+};
---
+++ Task8_3/Task8_3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "windows.h"
+#include "MyUniquePtr.cpp"
+//template<typename T>
+class MyClass {
+
+public:
+	MyClass(int n) : n(n) {}
+	int get_n() const {
+		std::cout << "Я объект класса MyClass и я вернул значение поля n через метод get_n()" << std::endl;
+		return n;
+	}
+private:
+	int n;
+};
+
+void print_my_class(MyUniquePtr<MyClass>& my_unique_ptr) {
+	MyClass temp = *my_unique_ptr;
+	std::cout << temp.get_n() << std::endl;
+}
+int main() {
+	SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+	SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
+	MyUniquePtr<MyClass> my_unique_ptr(new MyClass(10));
+	print_my_class(my_unique_ptr);
+	MyClass* temp = my_unique_ptr.release();
+	std::cout << temp->get_n() << std::endl;
+
+return 0;
+}
